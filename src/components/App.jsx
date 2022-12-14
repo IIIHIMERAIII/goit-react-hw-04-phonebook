0fd9434c0502c4d06276c4647e53ec1bc544681e
Maksym <PTNPNH722@gmail.com>
2022-12-14 11:39:50 +0200

    hooks update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,92 +3,73 @@ import { Section } from './section/section';
 import { Form } from './contactForm/form';
 import { Filter } from './filter/filter';
 import { Phonebook } from "./phonebook/phonebook";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends React.Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '4591256' },
-      { id: 'id-2', name: 'Hermione Kline', number: '4438912' },
-      { id: 'id-3', name: 'Eden Clements', number: '6451779' },
-      { id: 'id-4', name: 'Annie Copeland', number: '2279126' },
-    ],
-    filter: '',
-  };
+// let contactsData = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '4591256' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '4438912' },
+//   { id: 'id-3', name: 'Eden Clements', number: '6451779' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '2279126' },
+// ];
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const contactsFromLS = JSON.parse(localStorage.getItem("CONTACTS"));
+    return contactsFromLS ? contactsFromLS : [];
+  });
+  const [filter, setFilter] = useState("");
 
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem("CONTACTS"));
-    if (contacts) {
-      this.setState({ contacts });
-    }
-  };
+  useEffect(() => { localStorage.setItem("CONTACTS", JSON.stringify(contacts)) }, [contacts]);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("CONTACTS", JSON.stringify(this.state.contacts));
-    }
-  };
-
-  addContact = contact => {
+  const addContact = contact => {
     if (
-      this.state.contacts.find(
+      contacts.find(
         cont => cont.name.toLowerCase() === contact.name.toLowerCase()
       )
     ) {
       return alert(`${contact.name} is already in contacts`);
     }
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+    setContacts(prevState => [...prevState, contact]);
   };
 
-  onDelete = id => {
-    const newArray = this.state.contacts.filter(c => c.id !== id);
-    this.setState(prevState => ({
-      contacts: [...newArray],
-    }));
+  const onDelete = id => {
+    setContacts(contacts.filter(c => c.id !== id));
   };
 
-  onChange = event => {
-    const { name, value } = event.currentTarget;
-    this.setState({ [name]: value });
+  const onFilterChange = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  onFilterChange = ({ target: { value } }) => {
-    this.setState({ 'filter': value });
-  };
-
-  getFilterContacts = () => {
-    const { contacts, filter } = this.state;
-
-    return contacts.filter(el =>
-      el.name.toLowerCase().includes(filter.toLowerCase())
+  const onFilter = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-
-  render() {
-    const filterContacts = this.getFilterContacts();
-    return (
-      <div>
-        <Section title='Phonebook'>
-          <Form
-            onSubmit={this.addContact}
-          >
-          </Form>
-          <Section title='Contacts'>
-            <Filter
-              onChange={this.onFilterChange}
-              value={this.state.filter}
-            />
-            <Phonebook
-              data={this.state.contacts}
-              onDelete={this.onDelete}
-              filter={filterContacts}
-            />
-          </Section>
+  const filterContacts = onFilter();
+  return (
+    <div>
+      <Section title='Phonebook'>
+        <Form
+          onSubmit={addContact}
+        >
+        </Form>
+        <Section title='Contacts'>
+          <Filter
+            onChange={onFilterChange}
+            value={filter}
+          />
+          <Phonebook
+            data={contacts}
+            onDelete={onDelete}
+            filter={filterContacts}
+          />
         </Section>
-      </div>
-    );
-  };
+      </Section>
+    </div>
+  );
 };
+
+export { App };
